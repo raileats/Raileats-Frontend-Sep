d2fd37b082e19d5e7e0b32a4a431095367b38a3d
raileats <raileats@gmail.com>
2025-08-31 01:11:42 +0530

    Delete app directory

--- app/about/page.tsx
+++
@@ -1,1 +0,0 @@
-export default function About(){return <div className='p-6'><h1 className='text-2xl font-bold'>About Us</h1><p>Content coming soon...</p></div>}
--- app/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-black text-white text-center p-4 mt-10">
-      <p>© {new Date().getFullYear()} Raileats.in - Ab Rail Journey ka Swad Only Raileats ke Saath</p>
-    </footer>
-  )
-}
