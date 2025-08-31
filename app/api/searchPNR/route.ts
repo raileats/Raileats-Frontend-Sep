423189513e9b61929ab4361744aca0e174ed7604
raileats <raileats@gmail.com>
2025-08-31 09:47:46 +0530

    Delete app directory

--- app/api/searchPNR/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || '';
-  const res = await fetch(`${baseUrl}/data/dummyData.json`);
-  const data = await res.json();
-  return NextResponse.json(data);
-}
