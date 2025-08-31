bf6c676ab6da770d15e6738a7faa11ce8bb34422
raileats <raileats@gmail.com>
2025-08-31 13:01:36 +0530

    Create page.tsx

---
+++ app/components/page.tsx
@@ -0,0 +1,11 @@
+import HeroSlider from "./components/HeroSlider"
+import SearchBox from "./components/SearchBox"
+
+export default function Home() {
+  return (
+    <main>
+      <HeroSlider />
+      <SearchBox />
+    </main>
+  )
+}
