4cf5306cb045acfec2f76b36d774207047b9b0d6
raileats <raileats@gmail.com>
2025-08-31 13:09:46 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,17 @@
-import HeroSlider from "./components/HeroSlider";
-import SearchBox from "./components/SearchBox";
+import HeroSlider from "./components/HeroSlider"
+import SearchBox from "./components/SearchBox"
+import Offers from "./components/Offers"
+import Steps from "./components/Steps"
+import ExploreRailInfo from "./components/ExploreRailInfo"
 
 export default function Home() {
   return (
-    <main>
-      {/* Banner/Slider */}
+    <>
       <HeroSlider />
-
-      {/* Search Section */}
       <SearchBox />
-
-      {/* Content */}
-      <div className="p-6 text-center">
-        <h1 className="text-3xl font-bold">Welcome to Raileats.in</h1>
-        <p className="mt-2 text-lg">Ab Rail Journey ka Swad Only Raileats ke Saath</p>
-      </div>
-    </main>
-  );
+      <Offers />
+      <Steps />
+      <ExploreRailInfo />
+    </>
+  )
 }
