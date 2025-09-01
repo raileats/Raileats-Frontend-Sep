cfbb378cc31c32f77837bbbe3cb1b7538f938335
raileats <raileats@gmail.com>
2025-09-01 18:43:19 +0530

    Update BottomNav.tsx

--- app/components/BottomNav.tsx
+++ app/components/BottomNav.tsx
@@ -1,41 +1,31 @@
 "use client";
-
 import Link from "next/link";
 import Image from "next/image";
 
+
 export default function BottomNav() {
-  return (
-    <nav className="fixed bottom-0 left-0 w-full bg-white border-t border-gray-300 shadow-md flex justify-around items-center py-2 z-50 md:hidden">
-      <Link href="/" className="flex flex-col items-center text-sm text-gray-700">
-        <span className="text-xl">🏠</span>
-        <span>Home</span>
-      </Link>
+return (
+<div className="fixed bottom-0 left-0 w-full bg-white border-t border-gray-300 shadow-inner flex justify-around items-center h-16 md:hidden">
+<Link href="/" className="flex flex-col items-center text-xs">
+<span>Home</span>
+</Link>
+<Link href="/train-tools" className="flex flex-col items-center text-xs">
+<span>Train Tools</span>
+</Link>
 
-      <Link href="/train-tools" className="flex flex-col items-center text-sm text-gray-700">
-        <span className="text-xl">🚆</span>
-        <span>Train</span>
-      </Link>
 
-      {/* 🔥 Center Logo */}
-      <Link href="/" className="flex flex-col items-center">
-        <Image
-          src="/logo.png"  // public/logo.png में logo डालो
-          alt="RailEats Logo"
-          width={40}
-          height={40}
-          className="rounded-full"
-        />
-      </Link>
+{/* Floating logo in center */}
+<div className="relative -top-6 bg-yellow-400 rounded-full p-3 shadow-lg border-4 border-white">
+<Image src="/logo.png" alt="RailEats Logo" width={40} height={40} />
+</div>
 
-      <Link href="/offers" className="flex flex-col items-center text-sm text-gray-700">
-        <span className="text-xl">🎁</span>
-        <span>Offers</span>
-      </Link>
 
-      <Link href="/orders" className="flex flex-col items-center text-sm text-gray-700">
-        <span className="text-xl">📦</span>
-        <span>Orders</span>
-      </Link>
-    </nav>
-  );
+<Link href="/offers" className="flex flex-col items-center text-xs">
+<span>Offers</span>
+</Link>
+<Link href="/menu" className="flex flex-col items-center text-xs">
+<span>Menu</span>
+</Link>
+</div>
+);
 }
