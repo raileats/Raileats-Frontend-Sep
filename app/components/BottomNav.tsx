ffd3b3d0573137dc93aa22224fd79f4bb7047864
raileats <raileats@gmail.com>
2025-09-01 19:11:04 +0530

    Update BottomNav.tsx

--- app/components/BottomNav.tsx
+++ app/components/BottomNav.tsx
@@ -1,31 +1,34 @@
 "use client";
 import Link from "next/link";
-import Image from "next/image";
-
+import { Home, Train, Gift, ShoppingBag, Menu } from "lucide-react";
 
 export default function BottomNav() {
-return (
-<div className="fixed bottom-0 left-0 w-full bg-white border-t border-gray-300 shadow-inner flex justify-around items-center h-16 md:hidden">
-<Link href="/" className="flex flex-col items-center text-xs">
-<span>Home</span>
-</Link>
-<Link href="/train-tools" className="flex flex-col items-center text-xs">
-<span>Train Tools</span>
-</Link>
+  return (
+    <nav className="fixed bottom-0 left-0 w-full bg-white shadow-lg border-t flex justify-around items-center py-2 md:hidden">
+      <Link href="/" className="flex flex-col items-center text-gray-700">
+        <Home size={22} />
+        <span className="text-xs">Home</span>
+      </Link>
 
+      <Link href="/train-tools" className="flex flex-col items-center text-gray-700">
+        <Train size={22} />
+        <span className="text-xs">Train Tools</span>
+      </Link>
 
-{/* Floating logo in center */}
-<div className="relative -top-6 bg-yellow-400 rounded-full p-3 shadow-lg border-4 border-white">
-<Image src="/logo.png" alt="RailEats Logo" width={40} height={40} />
-</div>
+      {/* 🚀 Bubble Style Center Button */}
+      <div className="relative -mt-8 bg-yellow-500 rounded-full p-4 shadow-lg">
+        <Image src="/logo.png" alt="RailEats Logo" width={40} height={40} />
+      </div>
 
+      <Link href="/offers" className="flex flex-col items-center text-gray-700">
+        <Gift size={22} />
+        <span className="text-xs">Offers</span>
+      </Link>
 
-<Link href="/offers" className="flex flex-col items-center text-xs">
-<span>Offers</span>
-</Link>
-<Link href="/menu" className="flex flex-col items-center text-xs">
-<span>Menu</span>
-</Link>
-</div>
-);
+      <Link href="/menu" className="flex flex-col items-center text-gray-700">
+        <Menu size={22} />
+        <span className="text-xs">Menu</span>
+      </Link>
+    </nav>
+  );
 }
