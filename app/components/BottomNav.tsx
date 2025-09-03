9cb5690d4e150fcd2f40e752b5014a814caf19c2
raileats <raileats@gmail.com>
2025-09-04 02:33:19 +0530

    Update BottomNav.tsx

--- app/components/BottomNav.tsx
+++ app/components/BottomNav.tsx
@@ -9,32 +9,41 @@ export default function BottomNav() {
   return (
     <>
       {/* ✅ Bottom Fixed Navbar */}
-      <nav className="fixed bottom-0 left-0 w-full bg-white border-t shadow-lg flex justify-around items-center h-16 z-40">
-        <button className="flex flex-col items-center text-xs">
-          <Home size={22} />
+      <nav className="fixed bottom-0 left-0 w-full bg-white border-t shadow-lg flex justify-between items-center h-16 px-4 z-40">
+        {/* Home */}
+        <button className="flex flex-col items-center justify-center text-xs text-gray-700">
+          <Home size={22} className="text-blue-500" />
           Home
         </button>
 
-        <button className="flex flex-col items-center text-xs">
-          <Train size={22} />
+        {/* Train */}
+        <button className="flex flex-col items-center justify-center text-xs text-gray-700">
+          <Train size={22} className="text-green-500" />
           Train Tools
         </button>
 
-        {/* ✅ Center Bubble Partner Button */}
-        <button
-          onClick={() => setPartnerOpen(true)}
-          className="absolute -top-6 left-1/2 -translate-x-1/2 bg-yellow-400 rounded-full w-16 h-16 flex items-center justify-center shadow-lg border-4 border-white"
-        >
-          <img src="/logo.png" alt="RailEats Logo" className="w-9 h-9" />
-        </button>
+        {/* ✅ Center Partner Bubble */}
+        <div className="flex flex-col items-center">
+          <button
+            onClick={() => setPartnerOpen(true)}
+            className="relative bg-yellow-400 rounded-full w-16 h-16 flex items-center justify-center shadow-lg border-4 border-white -mt-8"
+          >
+            <img src="/logo.png" alt="RailEats Logo" className="w-9 h-9" />
+          </button>
+          <span className="text-xs font-semibold text-yellow-600 mt-1">
+            Partner
+          </span>
+        </div>
 
-        <button className="flex flex-col items-center text-xs">
-          <Gift size={22} />
+        {/* Offers */}
+        <button className="flex flex-col items-center justify-center text-xs text-gray-700">
+          <Gift size={22} className="text-pink-500" />
           Offers
         </button>
 
-        <button className="flex flex-col items-center text-xs">
-          <Menu size={22} />
+        {/* My Menu */}
+        <button className="flex flex-col items-center justify-center text-xs text-gray-700">
+          <Menu size={22} className="text-purple-500" />
           My Menu
         </button>
       </nav>
