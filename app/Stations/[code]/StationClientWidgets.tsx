c6d0917fce5ee683adc1f4e782a71b25c0ca68ab
raileats <raileats@gmail.com>
2025-10-20 15:05:25 +0530

    Update StationClientWidgets.tsx

--- app/Stations/[code]/StationClientWidgets.tsx
+++ app/Stations/[code]/StationClientWidgets.tsx
@@ -1,12 +1,12 @@
 // app/Stations/[code]/StationClientWidgets.tsx
 "use client";
 
-import StationSearchBox from "@/app/components/StationSearchBox";
+import StationSearchBox from "../../components/StationSearchBox";
 
 export default function StationClientWidgets() {
   return (
     <div className="mb-6">
-      <StationSearchBox onSelect={() => { /* no-op here; parent page will handle main navigation via SearchBox component elsewhere */ }} />
+      <StationSearchBox onSelect={() => { /* no-op here */ }} />
     </div>
   );
 }
