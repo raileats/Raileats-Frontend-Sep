bdbcb32613f7a2c467c81ee6e8e5d3457ef1698b
raileats <raileats@gmail.com>
2025-10-21 21:13:35 +0530

    Update StationClientWidgets.tsx

--- app/Stations/[code]/StationClientWidgets.tsx
+++ app/Stations/[code]/StationClientWidgets.tsx
@@ -1,12 +1,4 @@
 // app/Stations/[code]/StationClientWidgets.tsx
 "use client";
-
 import StationSearchBox from "../../components/StationSearchBox";
-
-export default function StationClientWidgets() {
-  return (
-    <div className="mb-6">
-      <StationSearchBox onSelect={() => { /* no-op here */ }} />
-    </div>
-  );
-}
+export default function StationClientWidgets(){ return <div className="mb-6"><StationSearchBox/></div>; }
