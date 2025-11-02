e4d66d176f3f46f677c00dd493a44d3795cd9d68
raileats <raileats@gmail.com>
2025-11-02 12:41:52 +0530

    Delete app/Stations/[code] directory

--- app/Stations/[code]/StationClientWidgets.tsx
+++
@@ -1,4 +0,0 @@
-// app/Stations/[code]/StationClientWidgets.tsx
-"use client";
-import StationSearchBox from "../../components/StationSearchBox";
-export default function StationClientWidgets(){ return <div className="mb-6"><StationSearchBox/></div>; }
