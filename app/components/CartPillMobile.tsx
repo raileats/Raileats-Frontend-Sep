872a18e61af19df5d6c2f9deb8080ad58b9ca2ca
raileats <raileats@gmail.com>
2026-04-26 23:22:37 +0530

    Update CartPillMobile.tsx

--- app/components/CartPillMobile.tsx
+++ app/components/CartPillMobile.tsx
@@ -25,7 +25,7 @@ export default function CartPillMobile() {
   if (!count || count === 0 || !show) return null;
 
   return (
-    <div className="fixed bottom-6 left-1/2 -translate-x-1/2 z-50 md:hidden">
+   <div className="fixed bottom-[80px] left-1/2 -translate-x-1/2 z-50 md:hidden">
 
       <button
         onClick={openCart}
