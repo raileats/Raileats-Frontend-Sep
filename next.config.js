006b7044d39680f8311777dd736b1bbc2ef17bc5
raileats <raileats@gmail.com>
2026-04-29 04:32:37 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,39 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+
+  compress: true,
+
+  images: {
+    formats: ["image/avif", "image/webp"],
+  },
+
+  experimental: {
+    optimizeCss: true,
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/:all*(js|css)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+      {
+        source: "/:all*(png|jpg|jpeg|webp|avif|svg)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
