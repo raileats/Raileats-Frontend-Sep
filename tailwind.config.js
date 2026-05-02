ae37f72b801d6ca8953c98c544c36d1c36d50b39
raileats <raileats@gmail.com>
2026-05-03 04:44:22 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,19 +6,7 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: "#EAB308",   // yellow-500
-        primaryDark: "#CA8A04",
-        textMain: "#111827",
-        textSub: "#6B7280",
-        borderLight: "#E5E7EB",
-        bgLight: "#F9FAFB",
-      },
-      borderRadius: {
-        xl2: "14px",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
