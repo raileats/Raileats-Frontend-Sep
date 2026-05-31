8bec7e583be747a38a66b5b0fc59cf2290a1148a
raileats <raileats@gmail.com>
2026-05-31 20:12:53 +0530

    Update RestroMenuClient.tsx

--- app/stations/[slug]/[restroSlug]/RestroMenuClient.tsx
+++ app/stations/[slug]/[restroSlug]/RestroMenuClient.tsx
@@ -337,6 +337,7 @@ export default function RestroMenuClient({
               <span style={{ width: 18, textAlign: "center" }}>📍</span>
               <span style={{ minWidth: 0, overflowWrap: "anywhere" }}>
                 {displayStationName || "-"}
+                {displayStationCode ? ` (${displayStationCode})` : ""}
               </span>
             </div>
           </div>
