de7c77f930da5fb8719f6a7802881a75d6ebca14
raileats <raileats@gmail.com>
2026-06-01 22:15:51 +0530

    Update page.tsx

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -336,6 +336,7 @@ useEffect(() => {
   readOnly={isPnrLocked}
   onChange={(e) => setPnr(e.target.value)}
 />
+</div>>
 
             {/* LAST ROW: COACH + SEAT + PROMO */}
             <div className="flex items-center gap-2 w-full">
