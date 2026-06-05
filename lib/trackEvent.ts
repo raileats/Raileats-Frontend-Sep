93192fc2a262f56fb6f0c917ee0b1b9ec023c82d
raileats <raileats@gmail.com>
2026-06-05 17:09:23 +0530

    Update trackEvent.ts

--- lib/trackEvent.ts
+++ lib/trackEvent.ts
@@ -36,10 +36,10 @@ function getBrowser() {
 
   const ua = navigator.userAgent;
 
+  if (ua.includes("Edg")) return "Edge";
   if (ua.includes("Chrome")) return "Chrome";
   if (ua.includes("Firefox")) return "Firefox";
   if (ua.includes("Safari")) return "Safari";
-  if (ua.includes("Edge")) return "Edge";
 
   return "Other";
 }
