d49fde888ae70b4f643a23a2a8d0de7dc85cba76
raileats <raileats@gmail.com>
2026-06-26 01:10:45 +0530

    Use Firebase env variables
    Updated Firebase configuration to use environment variables for sensitive data.

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB2e5hXGmUVaV390OlX_pJHwSWNPLtMm0Ic",
-  authDomain: "raileats-4be89.firebaseapp.com",
-  projectId: "raileats-4be89",
-  storageBucket: "raileats-4be89.firebasestorage.app",
-  messagingSenderId: "22051437827",
-  appId: "1:22051437827:web:fc242bc42ada9c0b74bd36",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
