fa7fc0fcc928f341c943cfb30f49eedd1d660f3f
raileats <raileats@gmail.com>
2026-07-29 03:59:20 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,6 +9,13 @@ const nextConfig = {
   images: {
     formats: ["image/avif", "image/webp"],
     minimumCacheTTL: 31536000,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ygisiztmuzwxpnvhwrmr.supabase.co",
+        pathname: "/storage/v1/object/public/**",
+      },
+    ],
   },
 
   async redirects() {
